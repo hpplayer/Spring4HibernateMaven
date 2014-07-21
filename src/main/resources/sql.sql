29086d90efc0fa6f9dcc9732f496986b03ffc5fb
hpplayer <hpplayer@live.cn>
2014-07-21 16:41:39 +0800

    add sql
    add sql

---
+++ src/main/resources/sql.sql
@@ -0,0 +1,10 @@
+CREATER DATABSE contact
+CREATE TABLE CONTACTS
+(
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    firstname    VARCHAR(30),
+    lastname    VARCHAR(30),
+    telephone   VARCHAR(15),
+    email         VARCHAR(30),
+    created     TIMESTAMP DEFAULT NOW()
+);
